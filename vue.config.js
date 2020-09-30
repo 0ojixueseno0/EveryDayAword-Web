df8a6b822fb823d82cc6d3b7d0ad9cea2cbec8c3
0ojixueseno0 <jz0ojiang@qq.com>
2020-09-30 16:40:48 +0800

    设置跨域 修复http请求方法

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://abc.im0o.top:14771",
+        //   ws: true,
+        changeOrigin: true,
+        pathRewrite: { "^/api": "" }
+      }
+    }
+  }
+};
